8e59cb8e654f6516f62a7cd170a3acf62fed11c2
Vova Khrystenko <ovod88@bigmir.net>
2018-07-02 15:35:32 -0700

    Added table for groups

--- students/models.py
+++ students/models.py
@@ -111,3 +111,30 @@ class Student(models.Model):
 
 	#JUST TASTE CUSTOM MANAGER WITH TICKET INT CONVERSION
 	students = TicketIntManager()
+
+
+class Group(models.Model):
+	class Meta(object):
+		verbose_name = u"Група"
+		verbose_name_plural = u"Групи"
+
+	title = models.CharField(
+		max_length=256,
+		blank=False,
+		verbose_name=u"Назва")
+	
+	leader = models.OneToOneField("Student",
+		verbose_name=u"Староста",
+		blank=True,
+		null=True,
+		on_delete=models.SET_NULL)
+
+	notes = models.TextField(
+		blank=True,
+		verbose_name=u"Додаткові нотатки")
+	
+	def __str__(self):
+		if self.leader:
+			return "{} ({} {})".format(self.title, self.leader.first_name,self.leader.last_name)
+		else:
+			return "{}".format(self.title,)
