7c60ce53657a45e5010793653b24399111533a17
Vova Khrystenko <ovod88@bigmir.net>
2018-08-18 16:50:15 -0700

    Added leader student validation in admin

--- students/admin.py
+++ students/admin.py
@@ -29,6 +29,19 @@ class ExaminResultForm(forms.ModelForm):
 class ExaminResultAdmin(admin.ModelAdmin):
 	form = ExaminResultForm
 
+
+class StudentFormAdmin(forms.ModelForm):
+	
+	def clean_student_group(self):
+		"""Check if student is leader in any group.
+			If yes, then ensure it’s the same as selected group."""
+		# get group where current student is a leader
+		groups = Group.objects.filter(leader=self.instance)
+		if len(groups) > 0 and self.cleaned_data['student_group'] != groups[0]:
+			raise forms.ValidationError(u'Студент є старостою іншої групи.', code='invalid')
+
+		return self.cleaned_data['student_group']
+
 class StudentAdmin(admin.ModelAdmin):
 	list_display = ['last_name', 'first_name', 'ticket', 'student_group']
 	list_display_links = ['last_name', 'first_name']
@@ -38,6 +51,7 @@ class StudentAdmin(admin.ModelAdmin):
 	list_per_page = 10
 	search_fields = ['last_name', 'first_name', 'middle_name', 'ticket','notes']
 	actions = ['copy_selected']
+	form = StudentFormAdmin
 
 	def copy_selected(self, request, queryset):
 		# print(queryset)
