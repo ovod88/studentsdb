28411ad2c086d6d9dbe2a286dd704c9a410ca9f0
Vova Khrystenko <ovod88@bigmir.net>
2018-09-11 06:06:58 -0700

    Added possibility to return to students page once saved edited student

--- students/static/js/home/main.js
+++ students/static/js/home/main.js
@@ -11,6 +11,8 @@ $(function(){
 		full_url = window.location.href,
 		post_url = full_url.substring(full_url.lastIndexOf('/'), full_url.length);
 
+    initEditStudentPage();
+
 	if($student_table.length) {
 		$button.on('click', function (e) {
 
@@ -67,17 +69,19 @@ $(function(){
                 									'/journal/' + student.id);
 
                 			$(student_html).appendTo($('.table > tbody')).show('slow');
-                            initEditStudentPage();
 
                 		}
 
                     }
+
+                    initEditStudentPage();
 
                     if(row_count > length || length == 0)  {
 
                     	$button.hide()
                     	$button.off('click');
                     	$(window).off('scroll');
+                        initEditStudentPage();
                     
                     }
 
@@ -99,4 +103,187 @@ $(function(){
 		}
 	}
 	
+    // initEditStudentPage();
 });
+
+
+function initEditStudentPage(event) {
+  // console.log('INITIATED STUDENT PAGE');
+
+  $('a.student-edit-form-link').off('click');
+
+  $('a.student-edit-form-link').click(function(event){
+    event.preventDefault();
+    var $link = $(this),
+        $loader_wrapper = $('.loader-wrapper'),
+        modal = $('#myModal'); 
+
+    // console.log('CLICKED');
+    // console.log($link[0].getAttribute('href'));
+
+    $loader_wrapper.fadeIn('slow'); 
+
+    setTimeout(function() {
+
+        // console.log(location.href);
+
+        // getModal($link, $loader_wrapper, modal);
+
+        getPageHtml($link.attr('href'), function(error, html) {
+
+          changeBrowserURL($link[0], location.href);
+
+          if(error) {
+
+            $loader_wrapper.fadeOut(100, function() {
+
+              modal.find('.modal-body').html('<p class="alert-danger">Помилка error на сервері. Спробуйте будь-ласка пізніше.</p>');
+              modal.find('.modal-footer').html('');
+              modal.find('.modal-title').html('');
+              modal.modal({
+                'keyboard': false,
+                'backdrop': false,
+                'show': true
+              });
+
+            });
+
+          } else {
+
+            $loader_wrapper.fadeOut(100, function() {
+
+              var $html = $(html), 
+                  form_html = $html.find('#content-column form');
+
+
+              modal.find('.modal-title').html($html.find('#content-column h2').text());
+              modal.find('.modal-body').html(form_html);
+
+              initEditStudentForm(form_html, modal);
+
+              modal.modal({
+                'keyboard': false,
+                'backdrop': false,
+                'show': true
+              }); 
+
+            });
+
+          }
+
+        });
+
+    }, 3000);
+
+  });
+
+}
+
+function initDatePicker() {
+
+  var $date_input_bt = $('button.dateinput'),
+        $input_date = $('.input-group input');
+
+    $date_input_bt.on('click', function() {
+
+      $input_date.datepicker({
+        format: 'yyyy-mm-dd',
+        autoclose: true,
+        language: 'uk'
+      }).focus();
+
+  });
+
+}
+
+function initEditStudentForm(form, modal) {
+
+  initDatePicker();
+  var $waiter = $('.waiter'),
+      $waiter_to_modal = $waiter.clone().removeAttr('style'),
+      $inputs = form.find('input'),
+      $select = form.find('select'),
+      $textarea = form.find('textarea'),
+      $modal_title = modal.find('.modal-title'),
+      $modal_body = modal.find('.modal-body'),
+      $modal_footer = modal.find('.modal-footer');
+
+  $waiter_to_modal.appendTo(modal.find('.modal-body'));
+
+  form.find('input[name="cancel_button"]').click(function(event){
+
+    event.preventDefault();
+    modal.modal('hide');
+
+  });
+
+  var $url = form.attr('action');
+
+  form.ajaxForm({
+    dataType : 'html',
+    beforeSend: function( xhr ) {
+      
+      $waiter_to_modal.animate({
+        opacity: 1
+      }, 100);
+      $inputs.prop('disabled', true);
+      $select.prop('disabled', true);
+      $textarea.prop('disabled', true);
+    },
+    error : function(){
+
+      setTimeout(function() {
+
+        $waiter_to_modal.animate({
+          opacity: 0
+        }, 100, function() {
+
+          $inputs.prop('disabled', false);
+          $select.prop('disabled', false);
+          $textarea.prop('disabled', false);
+          $modal_footer.html('<p class="alert-danger">Помилка i на сервері. Спробуйте будь-ласка пізніше.</p>');
+          
+          setTimeout(function() {
+            $modal_footer.html('');
+          }, 3000);
+
+        });
+
+      }, 4000);
+
+    },
+    success  : function(data, status, xhr) {
+
+      setTimeout(function() {
+
+        var html = $(data), 
+        newform = html.find('#content-column form');
+    
+        $modal_body.html(html.find('.alert'));
+        $modal_footer.html('');
+
+        // console.log(html);
+
+        if ( !newform.hasClass('delete-students') && newform.length > 0) {
+          
+          $modal_body.append(newform);
+
+          initEditStudentForm(newform, modal);
+
+        } else {
+
+          setTimeout(function(){
+            
+            location.href = history.state.prev_href;//REDIRECT TO PREVIOUS PAGE (URL IS STORED IN STATE WHILE PUSHSTATE)
+
+          }, 2000);
+
+        }
+
+      }, 3000);
+
+    }
+
+  });
+
+}
--- students/static/js/main.js
+++ students/static/js/main.js
@@ -20,120 +20,11 @@ function setGroupSelector() {
 
 }
 
-function initDatePicker() {
-
-  var $date_input_bt = $('button.dateinput'),
-        $input_date = $('.input-group input');
-
-    $date_input_bt.on('click', function() {
-
-      $input_date.datepicker({
-        format: 'yyyy-mm-dd',
-        autoclose: true,
-        language: 'uk'
-      }).focus();
-
-  });
-
-}
-
-function initEditStudentForm(form, modal) {
-
-  initDatePicker();
-  var $waiter = $('.waiter'),
-      $waiter_to_modal = $waiter.clone().removeAttr('style'),
-      $inputs = form.find('input'),
-      $select = form.find('select'),
-      $textarea = form.find('textarea'),
-      $modal_title = modal.find('.modal-title'),
-      $modal_body = modal.find('.modal-body'),
-      $modal_footer = modal.find('.modal-footer');
-
-  $waiter_to_modal.appendTo(modal.find('.modal-body'));
-
-  form.find('input[name="cancel_button"]').click(function(event){
-
-    event.preventDefault();
-    modal.modal('hide');
-
-  });
-
-  var $url = form.attr('action');
-
-  form.ajaxForm({
-    dataType : 'html',
-    beforeSend: function( xhr ) {
-      
-      $waiter_to_modal.animate({
-        opacity: 1
-      }, 100);
-      $inputs.prop('disabled', true);
-      $select.prop('disabled', true);
-      $textarea.prop('disabled', true);
-    },
-    error : function(){
-
-      setTimeout(function() {
-
-        $waiter_to_modal.animate({
-          opacity: 0
-        }, 100, function() {
-
-          $inputs.prop('disabled', false);
-          $select.prop('disabled', false);
-          $textarea.prop('disabled', false);
-          $modal_footer.html('<p class="alert-danger">Помилка i на сервері. Спробуйте будь-ласка пізніше.</p>');
-          
-          setTimeout(function() {
-            $modal_footer.html('');
-          }, 3000);
-
-        });
-
-      }, 4000);
-
-    },
-    success  : function(data, status, xhr) {
-
-      setTimeout(function() {
-
-        var html = $(data), 
-        newform = html.find('#content-column form');
-    
-        $modal_body.html(html.find('.alert'));
-        $modal_footer.html('');
-
-        // console.log(html);
-
-        if ( !newform.hasClass('delete-students') && newform.length > 0) {
-          
-          $modal_body.append(newform);
-
-          initEditStudentForm(newform, modal);
-
-        } else {
-
-          setTimeout(function(){
-            
-            location.reload(true);
-
-          }, 2000);
-
-        }
-
-      }, 3000);
-
-    }
-
-  });
-
-}
-
-function changeBrowserURL(dom_element) {
+function changeBrowserURL(dom_element, current_href=null) {
   // Change URL with browser address bar using the HTML5 History API.
   if (history.pushState) {
     // Parameters: data, page title, URL
-    history.pushState(null, null, dom_element.href);
+    window.history.pushState({ 'prev_href' : current_href }, '', dom_element ? dom_element.href : null );
   }
   // Fallback for non-supported browsers.
   else {
@@ -161,73 +52,6 @@ function getPageHtml(url, callback) {
 
 }
 
-function initEditStudentPage(event) {
-  console.log('INITIATED STUDENT PAGE');
-  $('a.student-edit-form-link').click(function(event){
-    event.preventDefault();
-    var $link = $(this),
-        $loader_wrapper = $('.loader-wrapper'),
-        modal = $('#myModal'); 
-
-    // console.log('CLICKED');
-    // console.log($link[0].getAttribute('href'));
-
-    $loader_wrapper.fadeIn('slow'); 
-
-    setTimeout(function() {
-
-        changeBrowserURL($link[0]);
-
-        // getModal($link, $loader_wrapper, modal);
-
-        getPageHtml($link.attr('href'), function(error, html) {
-
-          if(error) {
-
-            $loader_wrapper.fadeOut(100, function() {
-
-              modal.find('.modal-body').html('<p class="alert-danger">Помилка error на сервері. Спробуйте будь-ласка пізніше.</p>');
-              modal.find('.modal-footer').html('');
-              modal.find('.modal-title').html('');
-              modal.modal({
-                'keyboard': false,
-                'backdrop': false,
-                'show': true
-              });
-
-            });
-
-          } else {
-
-            $loader_wrapper.fadeOut(100, function() {
-
-              var $html = $(html), 
-                  form_html = $html.find('#content-column form');
-
-
-              modal.find('.modal-title').html($html.find('#content-column h2').text());
-              modal.find('.modal-body').html(form_html);
-
-              initEditStudentForm(form_html, modal);
-
-              modal.modal({
-                'keyboard': false,
-                'backdrop': false,
-                'show': true
-              }); 
-
-            });
-
-          }
-
-        });
-
-    }, 3000);
-
-  });
-
-}
-
 function popStateHandler() {
   // FF, Chrome, Safari, IE9.
   if (history.pushState) {
@@ -236,27 +60,42 @@ function popStateHandler() {
     //     alert(location.href);
     // }
 
-    window.addEventListener("popstate", function(e) {
+    window.onpopstate = function(e) {
+      // console.log(e.originalEvent);
+      alert('h');
+      // console.log(e);
 
-      // alert(location.href);
+      // location.href = location.href;
 
-      getPageHtml(location.href, function(error, html) {
+      // console.log(window.location);
+      // console.log(document.referrer);
+      // window.location = document.referrer;
+      if(e.state){
 
-        if(error) {
+        location.href = e.state.prev_href;
+      
+      } else {
 
-          return;
+        location.href = location.href;
 
-        } else {
+      }
+      // getPageHtml(location.href, function(error, html) {
 
-          var html_tag = document.getElementsByTagName("html")[0];
-          html_tag.innerHTML = html;
-          init();
+      //   if(error) {
 
-        }
+      //     return;
 
-      });
+      //   } else {
 
-    });
+      //     var html_tag = document.getElementsByTagName("html")[0];
+      //     html_tag.innerHTML = html;
+      //     init();
+
+      //   }
+
+      // });
+
+    };
   }
 }
 
@@ -281,6 +120,10 @@ function loadJournalPage() {
         method   : "GET",
         dataType : "html"
       }).then(function(data, status, xhr) {
+
+        changeBrowserURL($link.find('a')[0], current_href=location.href)
+
+        // console.log(history.state);
 
         var $html = $(data),
             $body = $('body'),
@@ -310,9 +153,9 @@ function loadJournalPage() {
         $('head').append($html.filter('link').not(".common"));
 
         $body.find('#scripts script').not('.common-script').remove();
-        $body.append($scripts);
+        $body.append($scripts);//jQuery will load the scripts but synchronously
 
-        initEditStudentPage();
+        // initEditStudentPage();
 
       }, function(xhr, status, error) {
 
@@ -334,7 +177,6 @@ function loadJournalPage() {
 function init() {
 
   setGroupSelector();
-  initEditStudentPage();
   loadJournalPage();
 
 }
@@ -343,6 +185,7 @@ $(function(){
 
   popStateHandler();
   init();
+  // window.history.replaceState({ 'prev_href' : location.href }, '', location.href );
   // initDatePicker();
 
 });
