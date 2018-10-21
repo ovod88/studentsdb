979443ac877bacd0ce69ae0712f5391a0c8d7e41
Vova Khrystenko <ovod88@bigmir.net>
2018-10-21 07:30:25 -0700

    Added tests for groups_context.py

---
+++ students/tests/test_context_processors.py
@@ -0,0 +1,19 @@
+from django.test import TestCase
+from django.http import HttpRequest
+
+from students.groups_context import groups_context_processor
+
+
+class ContextProcessorsTests(TestCase):
+
+    fixtures = ['fixture_data.json']
+
+    def test_groups_processor(self):
+        """Test groups processor"""
+        request = HttpRequest()
+        data = groups_context_processor(request)
+
+        # test data from processor
+        self.assertEqual(len(data['GROUPS_ALL']), 4)
+        self.assertEqual(data['GROUPS_ALL'][0]['str'], u'MTM-11 (Lina Khrystenko)')
+        self.assertEqual(data['GROUPS_ALL'][1]['str'], u'MTM-12 (Roman2 Demo)')
